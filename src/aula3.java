c87469c0c829d8976fa19d6b27004d344d38443e
maria <mclaradev@gmail.com>
2026-03-18 08:13:55 -0300

    aprendendo sobre quebra de linha e saída de dados

--- src/aula3.java
+++ src/aula3.java
@@ -1,4 +1,9 @@
     void main(){
+
+        int y = 32;
+        double x = 10.35784;
+        System.out.println(y);
+        System.out.println(x);
         System.out.print("Bom Dia! "); // sem quebra de linha
         System.out.println("Hello, World!"); // com quebra de linha
     }
