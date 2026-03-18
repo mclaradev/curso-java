2f4e03af894a93cc5a775f55a964d09036a654c9
maria <mclaradev@gmail.com>
2026-03-18 08:51:37 -0300

    concatenar vários elementos java

--- src/aula3.java
+++ src/aula3.java
@@ -2,6 +2,9 @@
 
         int y = 32;
         double x = 10.35784;
+        String nome = "Flora";
+        int age = 22;
+        double salary = 2000.0;
         System.out.println(y);
         System.out.println(x);
         System.out.printf("%.2f%n", x);
@@ -10,4 +13,5 @@
         System.out.printf("%.4f%n", x); // resultado com ponto
         System.out.print("Bom Dia! "); // sem quebra de linha
         System.out.println("Hello, World!"); // com quebra de linha
+        System.out.printf("%s tem %d anos de idade e ganha %.2f reais.", nome, age, salary);
     }
