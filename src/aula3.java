8fbe993f9c5f311253872688764a54656d264281
maria <mclaradev@gmail.com>
2026-03-18 08:22:58 -0300

    escrevendo conteúdo de uma variável com ponto flutuante

--- src/aula3.java
+++ src/aula3.java
@@ -4,6 +4,10 @@
         double x = 10.35784;
         System.out.println(y);
         System.out.println(x);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x); // resultado com vírgula
+        Locale.setDefault(Locale.US); // separa com ponto
+        System.out.printf("%.4f%n", x); // resultado com ponto
         System.out.print("Bom Dia! "); // sem quebra de linha
         System.out.println("Hello, World!"); // com quebra de linha
     }
