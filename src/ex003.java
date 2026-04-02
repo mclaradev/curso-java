53544244df8b1a569940a7ddf7d310f2909071da
maria <mclaradev@gmail.com>
2026-04-02 19:12:01 -0300

    fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.

---
+++ src/ex003.java
@@ -0,0 +1,12 @@
+public class ex003 {
+    public static void main(String[] args) {
+
+        int a = -10;
+
+        if (a >= 0){
+            System.out.printf("o número: %d NÃO É NEGATIVO!", a);
+        } else {
+            System.out.printf("o número: %d é NEGATIVO!", a);
+        }
+    }
+}
