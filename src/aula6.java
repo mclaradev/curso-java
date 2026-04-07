17421799a7133ba5c83b24e42bcb7d1338ee64d1
maria <mclaradev@gmail.com>
2026-04-06 21:14:19 -0300

    aula sobre: switch case

---
+++ src/aula6.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class aula6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        String dia;
+
+        switch (x) {
+            case 1:
+                dia = "Domingo";
+                break;
+            case 2:
+                dia = "Segunda-Feira";
+                break;
+            case 3:
+                dia = "Terça-Feira";
+                break;
+            case 4:
+                dia = "Quarta-Feira";
+                break;
+            case 5:
+                dia = "Quinta-Feira";
+                break;
+            case 6:
+                dia = "Sexta-Feira";
+                break;
+            case 7:
+                dia = "Sábado";
+                break;
+            default:
+                System.out.println("Mermão que dia é esse?");
+                break;
+        }
+
+        sc.close();
+    }
+}
