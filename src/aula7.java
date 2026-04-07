ee0d36848df47304f523e019e417e4ac145eab1f
maria <mclaradev@gmail.com>
2026-04-06 21:26:09 -0300

    COM expressão condicional ternária

--- src/aula7.java
+++ src/aula7.java
@@ -1,13 +1,15 @@
 public class aula7 {
     public static void main(String[] args) {
         double preco = 34.5;
-        double desconto;
+        double desconto = (preco <20.0) ? preco * 0.1 : preco * 0.05;
+
+       /* double desconto;
 
         if (preco < 20.0){
             desconto = preco * 0.1;
         } else {
             desconto = preco * 0.05;
-        }
+        } */
 
         System.out.println(desconto);
     }
